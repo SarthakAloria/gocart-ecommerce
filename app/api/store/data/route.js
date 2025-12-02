485e45921b54661700b5c1d28d3cc8e66ea6d639
Sarthak Aloria <141266376+SarthakAloria@users.noreply.github.com>
2025-12-02 20:17:32 +0530

    feat: add initial implementation of GET route for store info and products

---
+++ app/api/store/data/route.js
@@ -0,0 +1,12 @@
+
+
+// get store info & store products
+
+export async function GET(request) {
+    try {
+        // get store username from querry params
+        const { search}
+    } catch (error) {
+        
+    }
+}
