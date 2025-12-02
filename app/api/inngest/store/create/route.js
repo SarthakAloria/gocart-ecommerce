d90d3af901d65c7758f56a0645b2b28833d6938c
Sarthak Aloria <141266376+SarthakAloria@users.noreply.github.com>
2025-12-02 18:31:40 +0530

    feat: implement store creation logic with validation and user checks

--- app/api/inngest/store/create/route.js
+++ app/api/inngest/store/create/route.js
@@ -1,10 +1,48 @@
-import {getAuth} from '@clerk/nextjs/server';
+import prisma from '@/lib/prisma';
+import { getAuth } from '@clerk/nextjs/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req) {
+// Create the store
+export async function POST(request) {
     try {
-        const {userId} = getAuthh(request)
+        const { userId } = getAuth(request)
+        // Get the data from the form
         const formData = await request.formData()
+        const name = formData.get('name')
+        const username = formData.get('username')
+        const description = formData.get('description')
+        const email = formData.get('email')
+        const contact = formData.get('contact')
+        const address = formData.get('address')
+        const image = formData.get('image')
 
-    }catch (error) {
+        if (!name || !username || !description || !email || !contact || !address || !image) {
+            return NextResponse.json({ error: 'missing store info' }, { status: 400 })
+        }
+
+        // Check if the user already has a store
+        const store = await prisma.store.findFirst({
+            where: {
+                userId: userId
+            }
+        })
+
+        // If store is already registered then send status of store
+        if (store) {
+            return NextResponse.json({ store: store.status })
+        }
+
+        // Check if username is already taken
+        const isUsernameTaken = await prisma.store.findFirst({
+            where: { username: username.toLowerCase() }
+        })
+
+        if(isUsernameTaken) {
+            return NextResponse.json({error: "username already taken"}, {status: 400})
+        }
+
+        
+    } catch (error) {
+
     }
 }
